6f15a80f0e400cab6a2c2325052fea99151aacd1
Karcag Tamas <karcagtamas@outlook.com>
2025-09-26 11:15:16 +0200

    Add tauri plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,11 @@ pub fn run() {
     }];
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations("sqlite:app.bingo.db", migrations)
